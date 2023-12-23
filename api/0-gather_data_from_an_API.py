22b15289796dd84ad911c994866eadaeb161894e
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-12-23 20:23:10 +0100

    adding task 0

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+get data from a API
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    id = sys.argv[1]
+    completed = 0
+    total = 0
+    tasks = []
+    url_user = "https://jsonplaceholder.typicode.com/users/" + id
+    user = requests.get(url_user).json()
+    name = user.get("name")
+    todos = "https://jsonplaceholder.typicode.com/todos/"
+    task_list = requests.get(todos).json()
+
+    for task in task_list:
+        if task.get("userId") == int(id):
+            total += 1
+            if task.get("completed") is True:
+                tasks.append(task.get(task["title"]))
+                completed += 1
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, completed, total))
+
+    for t in tasks:
+        print('\t {}'.format(t))
