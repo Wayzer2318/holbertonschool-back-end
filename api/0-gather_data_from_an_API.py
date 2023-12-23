66e8b5805e9f3aae0d8ba6890cf69801a8afb29c
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-12-23 20:34:06 +0100

    modify the variable name

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -10,22 +10,19 @@ if __name__ == "__main__":
     id = sys.argv[1]
     completed = 0
     total = 0
-    tasks = []
+    task_title = []
     url_user = "https://jsonplaceholder.typicode.com/users/" + id
     user = requests.get(url_user).json()
     name = user.get("name")
     todos = "https://jsonplaceholder.typicode.com/todos/"
     task_list = requests.get(todos).json()
-
-    for task in task_list:
-        if task.get("userId") == int(id):
-            if task.get("completed") is True:
-                tasks.append(task["title"])
+    for t in task_list:
+        if t.get("userId") == int(id):
+            if t.get("completed") is True:
+                task_title.append(t["title"])
                 completed += 1
             total += 1
-
     print("Employee {} is done with tasks({}/{}):"
           .format(name, completed, total))
-
-    for t in tasks:
-        print('\t {}'.format(t))
+    for y in task_title:
+        print("\t {}".format(y))
